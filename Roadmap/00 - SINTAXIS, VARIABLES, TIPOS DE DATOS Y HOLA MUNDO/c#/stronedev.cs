7325bc108bff765a94533caea0c20d489b907d60
stronedev <120656214+yjesuslloro1135@users.noreply.github.com>
2024-12-03 16:45:16 +0100

    Update stronedev.cs

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/stronedev.cs
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/stronedev.cs
@@ -1,1 +1,19 @@
 using System
+class stronedev{
+public static void main
+
+// Documentación oficial: https://learn.microsoft.com/es-es/dotnet/csharp/
+
+// Comentario en una línea
+
+/* 
+Comentario en
+varias líneas
+*/
+
+string miVariable = "Mi variable";
+string miVariable = "Nuevo valor de mi variable";
+
+const miConstante = "Mi constante";
+
+Console.Write(miVariable); 
